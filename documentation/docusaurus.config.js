6b03de861e952039f7c195ee0f5490f61acb5c83
João <joaovcostaandrade@gmail.com>
2025-02-03 17:52:44 -0300

    ssssss

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
     
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 const organizationName = "Estagio-UniFil";
-const projectName = "Template";
+const projectName = "Template/documentation";
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Documentação Template',
